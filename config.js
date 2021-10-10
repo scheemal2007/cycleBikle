71bbde1844ffd5d516fb1ebaf389999681146116
scheemal2007 <74160474+scheemal2007@users.noreply.github.com>
2021-10-10 19:27:34 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAEz9-YvbHwgelQHHf0RoMmUvlaf9W_9xk",
+    authDomain: "wily-app-datastorer.firebaseapp.com",
+    projectId: "wily-app-datastorer",
+    storageBucket: "wily-app-datastorer.appspot.com",
+    messagingSenderId: "536814371245",
+    appId: "1:536814371245:web:7f022f39c4c00dfa292124"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore()
+
